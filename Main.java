67fa591a42a88d2202f03e1b4a39c7e6778b5613
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-01 13:35:43 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,17 +2,17 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		int kilo;
-		double boy;
+		int weight;
+		double height;
 		
-		Scanner girdi=new Scanner(System.in);
+		Scanner input=new Scanner(System.in);
 		System.out.print("Lütfen kilonuzu giriniz:");
-		kilo=girdi.nextInt();
+		weight=input.nextInt();
 		
 		System.out.print("Lütfen boyunuzu(metre cinsinde) giriniz:");
-		boy=girdi.nextDouble();
+		height=input.nextDouble();
 		
-		double vki=kilo/(boy*boy);
+		double vki=weight/(height*height);
 		
 	
 		System.out.println("Vücut kitle indeksiniz:" + vki);
